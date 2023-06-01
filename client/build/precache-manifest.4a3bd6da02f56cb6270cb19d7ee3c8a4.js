f909925e0e1ed381771bf370a04a95651eb32cc7
camilobit <camiloacevedovargas@gmail.com>
2023-06-01 08:40:13 -0500

    versión 12 construccion del build

---
+++ client/build/precache-manifest.4a3bd6da02f56cb6270cb19d7ee3c8a4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebd41cc52923accc6688",
+    "url": "/static/css/main.56bbfa0d.chunk.css"
+  },
+  {
+    "revision": "ebd41cc52923accc6688",
+    "url": "/static/js/main.ebd41cc5.chunk.js"
+  },
+  {
+    "revision": "2074423f1ea920086600",
+    "url": "/static/js/1.2074423f.chunk.js"
+  },
+  {
+    "revision": "0c0683826941190d24eb",
+    "url": "/static/js/2.0c068382.chunk.js"
+  },
+  {
+    "revision": "d4a1a7a4680295180dcc",
+    "url": "/static/js/runtime~main.d4a1a7a4.js"
+  },
+  {
+    "revision": "d123a461ab9af5310f59dfde4b5c9255",
+    "url": "/static/media/Portada de Libro de Cocina Fondo Foto Verduras.d123a461.png"
+  },
+  {
+    "revision": "14c66700c152d80f9eba813be1b4d15d",
+    "url": "/static/media/espinnerCargandoVerduras.14c66700.mp4"
+  },
+  {
+    "revision": "3236316b3d0b5f7728fa04b045c90071",
+    "url": "/static/media/All.3236316b.png"
+  },
+  {
+    "revision": "9843cd13244b93c1d6f372f8061c528a",
+    "url": "/static/media/glutenfree.9843cd13.png"
+  },
+  {
+    "revision": "e5ca787d2485ac8a404744e5b906d16d",
+    "url": "/static/media/dairyfree.e5ca787d.png"
+  },
+  {
+    "revision": "3c34a5bd40761febf8bdc331e3186d98",
+    "url": "/static/media/ketogenic.3c34a5bd.png"
+  },
+  {
+    "revision": "cdab6b7491848b2832ae4c453d81ed27",
+    "url": "/static/media/lactoOvo.cdab6b74.png"
+  },
+  {
+    "revision": "44f89f3a148c494188012f9f51e6f454",
+    "url": "/static/media/vegan.44f89f3a.png"
+  },
+  {
+    "revision": "d726be32f1bab40c631dbe4fc3bda320",
+    "url": "/static/media/pescatarian.d726be32.png"
+  },
+  {
+    "revision": "c621d034ea3de99e24aebcbdd642f1d0",
+    "url": "/static/media/paleolithic.c621d034.png"
+  },
+  {
+    "revision": "43efba32537765ceb6d6456ccaaecbbe",
+    "url": "/static/media/primal.43efba32.png"
+  },
+  {
+    "revision": "ce629649eb37ce3559c83e775141d3e6",
+    "url": "/static/media/30.ce629649.png"
+  },
+  {
+    "revision": "cdb57a9383bd14f1ab09035bbc5855e9",
+    "url": "/static/media/friendly.cdb57a93.png"
+  },
+  {
+    "revision": "66ed9d913efbcd7a0ad4badc5d0a8055",
+    "url": "/static/media/cargando2.66ed9d91.mp4"
+  },
+  {
+    "revision": "d346f6103dba881e543830b16201f602",
+    "url": "/static/media/videoLoader2.d346f610.mp4"
+  },
+  {
+    "revision": "3fff2b6537f442b067ea5c0ad2f1ea84",
+    "url": "/static/media/foto camarera.3fff2b65.jpg"
+  },
+  {
+    "revision": "fbbb0940d59c68e5387ac86338a26aea",
+    "url": "/static/media/foto_perfil_edit_link.fbbb0940.png"
+  },
+  {
+    "revision": "8c2ae09fab49145be30750f6f4855ef5",
+    "url": "/static/media/fondo animadode landingpage.8c2ae09f.mp4"
+  },
+  {
+    "revision": "be09c3639e5ac9f05aaba7cb16c25e83",
+    "url": "/static/media/fondoformulario.be09c363.avif"
+  },
+  {
+    "revision": "5382f467e00afc297f971eb786740872",
+    "url": "/static/media/doodle.5382f467.png"
+  },
+  {
+    "revision": "7036f950d4bd6b0516032d05087e7e52",
+    "url": "/index.html"
+  }
+];
